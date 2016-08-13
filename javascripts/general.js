c4a86e0fe409c0852fa3d008232225b8c062f91f
Ryan Storberg <ryan@storberg.net>
2016-08-13 16:54:05 -0600

    Add formspree

---
+++ javascripts/general.js
@@ -0,0 +1,15 @@
+// function adjustHeight(classType) {
+// 	var greatestHeight = 0;
+// 	$(classType).each(function(index, value) {
+// 		var itemHeight = $(value).height();
+// 		console.log(itemHeight);
+// 		if(itemHeight > greatestHeight){
+// 			greatestHeight = itemHeight;
+// 		}
+// 	});
+// 	$(classType).height(greatestHeight);
+// };
+//
+// $(document).on("ready", function() {
+// 	adjustHeight('.service');
+// });
