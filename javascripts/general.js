031c240f4b25ab51ae62c35dc2764fc5bceaef44
Ryan Storberg <ryan@storberg.net>
2018-11-22 14:05:03 +0000

    Misc

--- javascripts/general.js
+++ javascripts/general.js
@@ -1,4 +1,12 @@
-$('body').scrollspy({
-  target: '.navbar',
-  offset: 0
-});
+// window.onscroll = function() { makeNavStick() };
+//
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+//
+// function makeNavStick() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
