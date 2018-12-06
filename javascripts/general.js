14abb846241ed69df6ccd7afd484eb801ab8a840
Ryan Storberg <ryan@storberg.net>
2018-12-05 21:50:31 -0500

    Misc

--- javascripts/general.js
+++ javascripts/general.js
@@ -1,12 +1,1 @@
-// window.onscroll = function() { makeNavStick() };
-//
-// var navbar = document.getElementById("navbar");
-// var sticky = navbar.offsetTop;
-//
-// function makeNavStick() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
+
